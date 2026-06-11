fbbb51adc08172d5acb90f71449bba6d7e50b9f7
DJEUFACKWILSON <wilsondjeufack@gmail.com>
2026-06-11 14:08:22 +0100

    Switch database from MySQL to PostgreSQL
    Changed database engine from MySQL to PostgreSQL and updated database name.

--- backend/eatsy_project/settings.py
+++ backend/eatsy_project/settings.py
@@ -70,16 +70,8 @@ TEMPLATES = [{
 
 DATABASES = {
     'default': {
-        'ENGINE':   'django.db.backends.mysql',
-        'NAME':     config('DB_NAME',     default='eatsy_db'),
-        'USER':     config('DB_USER',     default='root'),
-        'PASSWORD': config('DB_PASSWORD', default=''),
-        'HOST':     config('DB_HOST',     default='127.0.0.1'),
-        'PORT':     config('DB_PORT',     default='3306'),
-        'OPTIONS': {
-            'charset':      'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
     }
 }
 
